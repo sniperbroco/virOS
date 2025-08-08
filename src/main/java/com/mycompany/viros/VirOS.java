5f5f7a12dab2fb84013b89c30a49c72f965299c4
delapenachristian <127620578+sniperbroco@users.noreply.github.com>
2025-08-08 16:06:06 +0800

    chore: add source files

---
+++ src/main/java/com/mycompany/viros/VirOS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.viros;
+
+/**
+ *
+ * @author chris
+ */
+public class VirOS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
